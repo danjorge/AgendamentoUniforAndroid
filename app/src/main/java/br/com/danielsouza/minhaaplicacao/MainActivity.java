1b865e7d0b5e6a6cfe36a626a4ef0766ef94452f
Daniel Jorge de Souza <danjorge@gmail.com>
2016-01-27 11:15:07 -0300

    sidemenu navigation drawer with rounded image

--- app/src/main/java/br/com/danielsouza/minhaaplicacao/MainActivity.java
+++ app/src/main/java/br/com/danielsouza/minhaaplicacao/MainActivity.java
@@ -1,85 +1,108 @@
 package br.com.danielsouza.minhaaplicacao;
 
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.WindowManager;
+import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.FrameLayout;
 
-import com.squareup.picasso.Picasso;
-
-import org.w3c.dom.Text;
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends NavigationDrawer {
 
-    NavigationView navView;
-    DrawerLayout drawerLayout;
-    ListView listView;
-    String[] opcoes = {"opcao1", "opcao2", "opcao3", "opcao4", "opcao5"};
-    ListViewAdapter adapter;
+    private TypedArray navMenuIcon;
+    private String[] navMenuName;
+    private ArrayList<br.com.danielsouza.minhaaplicacao.MenuItem> listMenuItens = new ArrayList<>();
+    private boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.base_layout);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        super.onCreateDrawer();
 
-        setTitle("");
+        navMenuIcon = getResources().obtainTypedArray(R.array.nav_drawer_icons);
+        navMenuName = getResources().getStringArray(R.array.nav_drawer_items);
 
-        listView = (ListView) findViewById(R.id.list_view);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        navView = (NavigationView) findViewById(R.id.nav_view);
+        listMenuItens.add(new br.com.danielsouza.minhaaplicacao.MenuItem(navMenuIcon.getResourceId(0, -1), navMenuName[0]));
+        listMenuItens.add(new br.com.danielsouza.minhaaplicacao.MenuItem(navMenuIcon.getResourceId(1, -1), navMenuName[1]));
+        listMenuItens.add(new br.com.danielsouza.minhaaplicacao.MenuItem(navMenuIcon.getResourceId(2, -1), navMenuName[2]));
+        listMenuItens.add(new br.com.danielsouza.minhaaplicacao.MenuItem(navMenuIcon.getResourceId(3, -1), navMenuName[3]));
+        listMenuItens.add(new br.com.danielsouza.minhaaplicacao.MenuItem(navMenuIcon.getResourceId(4, -1), navMenuName[4]));
 
-        adapter = new ListViewAdapter(this);
+        navMenuIcon.recycle();
+
+        adapter = new ListViewAdapter(this, listMenuItens);
         listView.setAdapter(adapter);
 
-        ImageView profileImage = (ImageView) findViewById(R.id.profile_image);
-        Picasso.with(this).load(R.drawable.image_usuario).into(profileImage);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        TextView profileLogin = (TextView) findViewById(R.id.profile_login);
-        profileLogin.setText("danjorge@gmail.com");
-
-        listView.setOnItemClickListener(new OnItemClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, "Item: " + opcoes[position], Toast.LENGTH_SHORT).show();
-                drawerLayout.closeDrawers();
-            }
-        });
+                switch (position) {
 
-        ImageView imgView = (ImageView)findViewById(R.id.img_drawer);
-        imgView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                    case 1:
+                        SolicitacoesFragment2 solicitacoes2 = new SolicitacoesFragment2();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.changeable, solicitacoes2).commit();
+                        break;
+
+                    case 3:
+                        SobreAplicacaoFragment sobreAplicacao = new SobreAplicacaoFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.changeable, sobreAplicacao).commit();
+                        break;
+
+                    case 4:
+                        dialogExitApplication(view);
+                        break;
+                }
+                updateMenuItens(position);
                 toogleMenu();
             }
         });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        setTitle("");
+
+        frameLayout = (FrameLayout) findViewById(R.id.changeable);
+
+        View actionB = findViewById(R.id.action_b);
+        actionB.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.unifor.br");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
             }
         });
+
+    }
+
+    public void finish(View view){
+        dialogExitApplication(view);
+    }
+
+    public void updateMenuItens(int position){
+        for(br.com.danielsouza.minhaaplicacao.MenuItem m : listMenuItens){
+            m.setSelected(false);
+        }
+        listMenuItens.get(position).setSelected(true);
+
+        adapter.notifyDataSetChanged();
     }
 
     @Override
@@ -100,11 +123,43 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void toogleMenu() {
-        if(drawerLayout.isDrawerOpen(navView)){
-            drawerLayout.closeDrawers();
-        } else {
-            drawerLayout.openDrawer(navView);
+    @Override
+    public void onBackPressed() {
+        if(doubleBackToExitPressedOnce){
+            super.onBackPressed();
+            return;
         }
+
+        this.doubleBackToExitPressedOnce = true;
+        View view = findViewById(R.id.changeable);
+        dialogExitApplication(view);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 10);
+    }
+
+    protected void dialogExitApplication(View view){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+        builder.setMessage("Tem certeza que deseja sair?")
+                .setCancelable(true)
+                .setNegativeButton("NÃO", new AlertDialog.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .setPositiveButton("SIM", new AlertDialog.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                        //error here. Intend to close the activtiy that created this dialog and has the error
+                    }
+                });
+
+        AlertDialog alert = builder.create();
+        // The dialog utils is outside an activity. Need to set owner
+        alert.show();
     }
 }
